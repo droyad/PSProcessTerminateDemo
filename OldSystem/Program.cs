722e782a259d8c625e262b160c36728a8011f7cc
Robert Wagner <robert.wagner@octopus.com>
2019-08-01 22:49:13 +0000

    Demo app

---
+++ OldSystem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace OldSystem
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            void WriteData(Action<string> action, ManualResetEventSlim resetEvent, DataReceivedEventArgs e)
+            {
+                try
+                {
+                    if (e.Data == null)
+                    {
+                        resetEvent.Set();
+                        return;
+                    }
+
+                    action(e.Data);
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Error occured handling message: {ex}");
+                    }
+                    catch
+                    {
+                        // Ignore
+                    }
+                }
+            }
+
+            using (var outputResetEvent = new ManualResetEventSlim(false))
+            using (var errorResetEvent = new ManualResetEventSlim(false))
+            using (var process = new Process())
+            {
+                Console.WriteLine("Starting child process");
+
+                var errorPref = args.FirstOrDefault() ?? "Stop";
+                var cmd = $@"$ErrorActionPreference = '{errorPref}'; & .\PrintErrors.exe";
+
+                process.StartInfo.FileName = @"C:\windows\system32\WindowsPowershell\v1.0\PowerShell.exe";
+                process.StartInfo.Arguments = $@"-Command ""{cmd}""";
+                process.StartInfo.WorkingDirectory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+
+                process.OutputDataReceived += (sender, e) => WriteData(Console.WriteLine, outputResetEvent, e);
+                process.ErrorDataReceived += (sender, e) => WriteData(Console.Error.WriteLine, errorResetEvent, e);
+
+                process.Start();
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+                process.CancelErrorRead();
+                process.CancelOutputRead();
+
+                outputResetEvent.Wait(CancellationToken.None);
+                errorResetEvent.Wait(CancellationToken.None);
+
+
+                return process.ExitCode;
+            }
+        }
+    }
+}
---
+++ PrintErrors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintErrors
+{
+    internal class Program
+    {
+        public static int Main()
+        {
+            Console.Error.WriteLine("One");
+            Console.Error.WriteLine("Two");
+            Thread.Sleep(1000);
+            Console.Error.WriteLine("Three");
+            return 7;
+        }
+    }
+}
